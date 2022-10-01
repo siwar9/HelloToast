9695fbf5dffbb8ffeb83f5fc34c077df588431c5
Siwar Ayed <ayedsiwar2443@gmail.com>
2022-10-01 09:59:45 +0100

    Update

--- app/src/main/java/com/example/android/MainActivity.java
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,7 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnCount = findViewById(R.id.button_count);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        btnCount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                countUp();
+            }
+        });
     }
 
     public void showToast(View view) {
@@ -24,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
         toast.show();
     }
 
-    public void countUp(View view) {
+    public void countUp() {
         ++mCount;
         if (mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
